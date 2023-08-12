39b18a06f189f8a296529e19392cd4c9c273a95e
ViVaKR <admin@vivabj.com>
2023-08-12 19:26:52 +0900

    Add While-Example-1

--- MSSQL/Snippets/While-Example-1.sql
+++ MSSQL/Snippets/While-Example-1.sql
@@ -1,6 +1,27 @@
+--* While Loop
+--! Declare Table Example
+USE PlayGround
+GO
+
+declare @TestTable Table (ColA INT, ColB CHAR(3));
+DECLARE @MyCounter INT
+SET @MyCounter = 0;
+
+WHILE (@MyCounter < 26)
+BEGIN
+    INSERT INTO @TestTable VALUES (@MyCounter, CHAR(@MyCounter + ASCII('a')));
+
+    SET @MyCounter = @MyCounter + 1;
+
+END
+
+SELECT
+    ColA, ColB
+FROM
+    @TestTable
 
 
---* Create PlayGround Table
+--* Create PlayGround Database
 USE master
 GO
 
